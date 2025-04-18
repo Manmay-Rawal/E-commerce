9dc8fe75b8d601ebf9a416952df9412daf931a46
Manmay Rawal <manmayrawal406@gmail.com>
2025-04-18 19:19:54 +0530

    add tailwind to front

--- front/src/Router/UseRoutes.jsx
+++ front/src/Router/UseRoutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
-import NavBar from '../component/NavBar.jsx'
+import NavBar from '../Coustomer/component/Navigation/Navigation.jsx'
 
 const UseRoutes = () => {
   return (
