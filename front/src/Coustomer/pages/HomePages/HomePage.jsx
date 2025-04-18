16f9965573699554fcbfaceebd3f00c6e8e76861
Manmay Rawal <manmayrawal406@gmail.com>
2025-04-18 20:53:50 +0530

    add carousel in Home pages

---
+++ front/src/Coustomer/pages/HomePages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainCarousel from '../../component/HomeCarousel/MainCarousel'
+
+const HomePage = () => {
+  return (
+    <>
+    <MainCarousel/>
+
+    <div>
+        Outher Section
+    </div>
+    </>
+  )
+}
+
+export default HomePage
--- front/src/Router/UseRoutes.jsx
+++ front/src/Router/UseRoutes.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import NavBar from '../Coustomer/component/Navigation/Navigation.jsx'
+import HomePage from '../Coustomer/pages/HomePages/HomePage.jsx'
 
 const UseRoutes = () => {
   return (
     <div>
         <Routes>
-            <Route path='/' element={<NavBar />}></Route>
+            <Route path='/' element={<><NavBar /><HomePage /></>}></Route>
         </Routes>
     </div>
   )
