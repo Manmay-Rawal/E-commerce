01013aa42edac0cfd33161d6f6bc265d7ff58aff
Manmay Rawal <manmayrawal406@gmail.com>
2025-04-18 22:04:54 +0530

    add carousel data to main carousel

---
+++ front/src/Coustomer/component/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,7 @@
+export const MainCarouselData = [
+    {
+        id: 1,
+        image: "https://www.himalayanbuzz.com/wp-content/uploads/2018/09/Top_Fashion_Week_India.jpg",
+        path:"/"
+    }
+]
