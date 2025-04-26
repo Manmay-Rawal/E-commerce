7b87b479da0599942f347269e3150afc0469bfc9
Manmay Rawal <manmayrawal406@gmail.com>
2025-04-26 12:45:03 +0530

    Add Data files for various categories in the front

--- front/src/Coustomer/component/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ front/src/Coustomer/component/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
 import AliceCarousel from "react-alice-carousel";
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import { Button } from "@mui/material";
+import { useState } from "react";
+
 
 const HomeSectionCarousel = () => {
+
+  const [ActiveIndex, setActiveIndex] = useState(0);
+
   const responsive = {
     0: { items: 1 },
     720: { items: 3 },
     1024: { items: 5.5 },
   };
 
-  const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+  const ActivePerv = () => {setActiveIndex(ActiveIndex - 1)};
+  const ActiveNext = () => {setActiveIndex(ActiveIndex + 1)};
+  const syncActiveIndex = (items) => {setActiveIndex(items)};
+;
+  const items = [1, 1, 1, 1, 1, 1,, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
 
   return (
-    <div className="relative lg:px-8 px-4 border border-black">
+    <div className="relative lg:px-8 px-4">
       <div className="relative p-5 ">
         <AliceCarousel
           items={items}
@@ -22,13 +31,43 @@ const HomeSectionCarousel = () => {
           infinite
           responsive={responsive}
           disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          ActiveIndex={ActiveIndex}
         />
-        <Button variant="contained" className="z-50" sx={{ position: "absolute", top: "8rem", right: "0", transform: "translateX(50%) rotate(90deg)", bgcolor:"white"}}area-label="right" >
-          <KeyboardArrowLeftIcon sx={{transform:"rotate(90deg)",color:"black"}}/>
-        </Button>
-        <Button variant="contained" className="z-50" sx={{ position: "absolute", top: "8rem", left: "0", transform: "translateX(50%) rotate(-90deg)", bgcolor:"white"}}area-label="right" >
-          <KeyboardArrowLeftIcon sx={{transform:"rotate(90deg)",color:"black"}}/>
-        </Button>
+        {ActiveIndex !== items.length - 5 && <Button
+          variant="contained"
+          className="z-50"
+          onClick={ActivePerv}
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            right: "0",
+            transform: "translateX(50%) rotate(90deg)",
+            bgcolor: "white",
+          }}
+          area-label="right"
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(90deg)", color: "black" }}
+          />
+        </Button>}
+        {ActiveIndex !== ActiveIndex+5 && <Button
+          variant="contained"
+          className="z-50"
+          onClick={ActiveNext}
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            left: "0",
+            transform: "translateX(-50%) rotate(-90deg)",
+            bgcolor: "white",
+          }}
+          area-label="right"
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(90deg)", color: "black" }}
+          />
+        </Button>}
       </div>
     </div>
   );
