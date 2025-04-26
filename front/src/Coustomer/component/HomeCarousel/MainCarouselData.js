7ef6dfe3fb2570572245af7a078ce2a2565b6799
Manmay Rawal <manmayrawal406@gmail.com>
2025-04-26 09:38:32 +0530

    corusel

--- front/src/Coustomer/component/HomeCarousel/MainCarouselData.js
+++ front/src/Coustomer/component/HomeCarousel/MainCarouselData.js
@@ -1,7 +1,22 @@
 export const MainCarouselData = [
-    {
-        id: 1,
-        image: "https://www.himalayanbuzz.com/wp-content/uploads/2018/09/Top_Fashion_Week_India.jpg",
-        path:"/"
-    }
-]
+  {
+    image:
+      "https://samyakkclothing.wordpress.com/wp-content/uploads/2023/05/01.lehenga-1200x6280000-1.jpg",
+    path: "/",
+  },
+  {
+    image:
+      "https://samyakkclothing.wordpress.com/wp-content/uploads/2023/05/01.lehenga-1200x6280000-1.jpg",
+    path: "/",
+  },
+  {
+    image:
+      "https://samyakkclothing.wordpress.com/wp-content/uploads/2023/05/01.lehenga-1200x6280000-1.jpg",
+    path: "/",
+  },
+  {
+    image:
+      "https://samyakkclothing.wordpress.com/wp-content/uploads/2023/05/01.lehenga-1200x6280000-1.jpg",
+    path: "/",
+  },
+];
