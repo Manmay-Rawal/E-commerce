1ab3af9c09be0e06746a523a3bbe4bbe72d2aeb0
Manmay Rawal <manmayrawal406@gmail.com>
2025-04-26 11:39:38 +0530

    add HomeSectionCarousel

--- front/src/Coustomer/component/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ front/src/Coustomer/component/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
-import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
-import AliceCarousel from 'react-alice-carousel';
+import React from "react";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import AliceCarousel from "react-alice-carousel";
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import { Button } from "@mui/material";
 
 const HomeSectionCarousel = () => {
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 5.5 },
+  };
 
-    const responsive = {
-        0: { items: 1 },
-        720: { items: 3 },
-        1024: { items: 4 },
-    };
-
-    const items = [1,1,1,1,1].map((item) => <HomeSectionCard />);
+  const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
 
   return (
-    <AliceCarousel 
-      items={items}
-    disableButtonsControls
-    autoPlay
-    autoPlayInterval={1000}
-    infinite
-    responsive={responsive}
-       />
-  )
-}
+    <div className="relative lg:px-8 px-4">
+      <div className="relative p-5 ">
+        <AliceCarousel
+          items={items}
+          disableButtonsControls
+          infinite
+          responsive={responsive}
+          disableDotsControls
+        />
+        <Button variant="contained" className="z-50" sx={{ position: "absolute", top: "8rem", right: "0", transform: "translateX(50%) rotate(90deg)", bgcolor:"white"}}area-label="right" >
+          <KeyboardArrowLeftIcon sx={{transform:"rotate(90deg)",color:"black"}}/>
+        </Button>
+        <Button variant="contained" className="z-50" sx={{ position: "absolute", top: "8rem", left: "0", transform: "translateX(50%) rotate(-90deg)", bgcolor:"white"}}area-label="right" >
+          <KeyboardArrowLeftIcon sx={{transform:"rotate(90deg)",color:"black"}}/>
+        </Button>
+      </div>
+    </div>
+  );
+};
 
-export default HomeSectionCarousel
+export default HomeSectionCarousel;
