507767f44603ffda13bf9c920abf7cf960fc6613
Manmay Rawal <manmayrawal406@gmail.com>
2025-04-28 17:18:08 +0530

    add Cart component and CartItem component

--- front/src/Coustomer/component/Cart/CartItem.jsx
+++ front/src/Coustomer/component/Cart/CartItem.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const CartItem = () => {
   return (
-    <div>CartItem</div>
+    <div className='p-5 shadow-lg rounded-md'>
+        <div className='flex items-center '>
+            <div className='w-[5rem] h-[5rem] lg:w-[9rem] lg:h-[9rem]'>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRARBIUEFIQhZATBAokBfxf1nrfqxv8lBOj1w&s" alt="" className="w-full h-full object-cover"/>
+            </div>
+        </div>
+    </div>
   )
 }
 
