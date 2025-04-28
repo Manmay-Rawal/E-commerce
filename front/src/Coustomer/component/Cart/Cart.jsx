6ab041377eab0c93a4c83bf53d5da562f1b159c8
Manmay Rawal <manmayrawal406@gmail.com>
2025-04-28 20:46:11 +0530

    add

--- front/src/Coustomer/component/Cart/Cart.jsx
+++ front/src/Coustomer/component/Cart/Cart.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import CartItem from './CartItem'
+import React from "react";
+import CartItem from "./CartItem";
 
 const Cart = () => {
   return (
     <div>
-        <CartItem/>
+      <div className="lg:grid grid-cols-3 lg:px-6 relative">
+        <div className="col-span-2">
+          <CartItem />
+        </div>
+        <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0">
+          <div className="shadow-lg">
+            <p className="uppercase font-bold opacity-60 pb-4">Price Details</p>
+            <hr />
+            <div className="space-y-3 font-semibold">
+              <div className="flex justify-between text-black pt-3">
+                <span>Price</span>
+                <span>₹4573</span>
+              </div>
+              <div className="flex justify-between text-black pt-3">
+                <span>Discount</span>
+                <span></span>
+              </div>
+              <div className="flex justify-between text-black pt-3">
+                <span>Dilivery Charge</span>
+                <span>₹4573</span>
+              </div>
+              <div className="flex justify-between text-black pt-3">
+                <span>Total Amount</span>
+                <span>₹4573</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
--- front/src/Coustomer/component/Cart/CartItem.jsx
+++ front/src/Coustomer/component/Cart/CartItem.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React from "react";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
 
 const CartItem = () => {
   return (
-    <div className='p-5 shadow-lg rounded-md'>
-        <div className='flex items-center '>
-            <div className='w-[5rem] h-[5rem] lg:w-[9rem] lg:h-[9rem]'>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRARBIUEFIQhZATBAokBfxf1nrfqxv8lBOj1w&s" alt="" className="w-full h-full object-cover"/>
-            </div>
+    <div className="p-5 shadow-lg rounded-md">
+      <div className="flex items-center ">
+        <div className="w-[5rem] h-[5rem] lg:w-[9rem] lg:h-[9rem]">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRARBIUEFIQhZATBAokBfxf1nrfqxv8lBOj1w&s"
+            alt=""
+            className="w-full h-full object-cover"
+          />
         </div>
-    </div>
-  )
-}
+        <div className="ml-5 space-y-1">
+          <p className="font-semibold">men selim mid persion also use</p>
+          <p className="opacity-70">Size:S wight</p>
+          <p className="opacity-70 mt-2"> seller:Crishtaliyo fashion</p>
 
-export default CartItem
+          <div className="flex space-x-5 items-center text-gray-500 pt-6">
+            <p className="font-semibold">199₹</p>
+            <p className="opacity-70 line-through">199₹</p>
+            <p className="font-semibold text-green-900">50% off</p>
+          </div>
+          <div className="lg:flex items-center lg:space-x-10 pt-4">
+            <div className="flex items-center space-x-2">
+              <IconButton>
+                <RemoveCircleOutlineIcon />
+              </IconButton>
+              <span className="py-1 px-7 rounded-sm border">3</span>
+              <IconButton sx={{ color:"RGB(145 85 253)"}}>
+                <AddCircleOutlineIcon />
+              </IconButton>
+            </div>
+            <div>
+              <Button sx={{ color:"RGB(145 85 253)"}}>
+                Remove
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
