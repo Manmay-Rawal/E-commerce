5294161fd762d919f58c66955ccec1c218f4306f
Manmay Rawal <manmayrawal406@gmail.com>
2025-05-03 14:43:17 +0530

    add model schema

---
+++ backend/src/model/user.models.js
@@ -0,0 +1,52 @@
+import mongoose, { model } from "mongoose";
+
+
+const userSchema = mongoose.Schema({
+    firstName:{
+        required:true,
+        type:String,
+    },
+    lastName:{
+        required:true,
+        type:String,
+    },
+    emailName:{
+        required:true,
+        type:String,
+    },
+    password:{
+        required:true,
+        type:String,
+    },
+    role:{
+        required:true,
+        type:String,
+        default:"CUSTOMER"
+    },
+    mobile:{
+        type:Number,
+    },
+    address:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"addresses"
+    }],
+    paymentsInformation:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"payments_information"
+    }],
+    ratings:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"ratings"
+    }],
+    reviews:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"reviews"
+    }],
+    createdAt:[{
+        type:Date,
+        default:Date.now()
+    }],
+})
+
+const User=mongoose.model("users",userSchema);
+module.exports=User;
