0491e149ec9f8b6ec632f2695f42038c45dfbb89
Manmay Rawal <manmayrawal406@gmail.com>
2025-05-05 12:44:22 +0530

    add cart-service

--- backend/src/services/cart.service.js
+++ backend/src/services/cart.service.js
@@ -1,4 +1,6 @@
 const Cart = require("../model/cart.models.js")
+const CartItems = require("../model/cartItems.models.js")
+const Product = require("../model/product.models.js")
 
 async function createCart(user) {
   try {
@@ -10,4 +12,58 @@ async function createCart(user) {
   }
 }
 
-module.exports =  { createCart };
+async function findUserCart(userId){
+  try {
+    let cart=await Cart.findOne({user:userId});
+    let cartItems = await CartItems.find({cart:cart._id}).populate("product")
+    cart.cartItems=cartItems;
+
+    let totalPrice=0;
+    let totalDiscountedPrice=0;
+    let totalItem=0;
+
+    for(let cartItems of cart.cartItems){
+      totalPrice += cartItems.price;
+      totalDiscountedPrice += cartItems.discountedPrice;
+      totalItem += cartItems.quantity;
+    }
+
+    cart.totalPrice=totalPrice;
+    cart.totalItem=totalItem;
+    cart.discounte=totalPrice-totalDiscountedPrice;
+
+    return cart
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+async function addCartItem(userId,req){
+  try {
+    const cart=await Cart.findOne({user:userId});
+    const product = await Product.findById(req.productId)
+
+    const isPresent = await CartItem.findOne({cart:cart._id,product:product._id,userId})
+
+    if(!isPresent){
+      const CartItem = new CartItem({
+        product:product._id,
+        cart:cart._id,
+        quantity:1,
+        userId,
+        price:product.price,
+        size:req.size,
+        discountedPrice:product.discountedPrice,
+      })
+
+      const createdCartItem=await cartItem.save();
+      cart.cartItems.push(createdCartItem);
+      await cart.save();
+      return "Item added to cart"
+    }
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+module.exports =  { createCart ,addCartItem,findUserCart};
---
+++ backend/src/services/cartItem.service.js
@@ -0,0 +1,37 @@
+const userService = require("../services/user.service.js")
+
+
+async function updateCartItem(userId,cartItemId,cartItemData){
+    try {
+        
+        const item=await findCartItemById(cartItemId);
+
+        if(!item){
+            throw new Error("cart item is not found : ",cartItemId)
+        }
+
+        const user =await userService.findUserById(item.userId)
+
+        if(!user){
+            throw new Error("user not found : ",userId)
+        }
+
+        if(user._id.toString()===userId.toString()){
+            item.quantity=cartItemData.quantity;
+            item.price=item.quantity*item.product.price;
+            item.discountedPrice=item.quantity*item.product.discountedPrice;
+
+            const updatedCartItem= await item.save();
+            return updatedCartItem;
+        }
+        else{
+            throw new Error("you can't update this cart item");
+        }
+
+    } catch (error) {
+        throw new Error(error.massage)
+    }
+}
+
+
+module.exports = {updateCartItem}
